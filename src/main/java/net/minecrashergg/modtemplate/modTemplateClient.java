42e89c53fca3f9cb1534a59420e5bcda09c51467
minecrasher <minecrasher@minecrasher.minecrasher>
2024-09-19 09:40:55 +0200

    Initial commit

---
+++ src/main/java/net/minecrashergg/modtemplate/modTemplateClient.java
@@ -0,0 +1,10 @@
+package net.minecrashergg.modtemplate;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class modTemplateClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
